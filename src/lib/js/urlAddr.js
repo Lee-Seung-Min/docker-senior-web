187134a0e6cf5085888ad486b74c9c0cfb391012
j.sohyun <j.sohyun@bit.kr>
2025-12-31 14:00:29 +0900

    불필요한 서버 url 삭제

--- src/lib/js/urlAddr.js
+++ src/lib/js/urlAddr.js
@@ -1,26 +1,3 @@
-// //로컬개발용
-// export let shopUrlAddr = "http://localhost:8000/shop";
-// export let authUrlAddr = "http://localhost:8000/auth";
-// export let mobileUrlAddr = "http://localhost:8000/mobile";
-// export let adminUrlAddr = "http://localhost:8000/admin";
-
-// export let shopUrlAddr = "https://122.199.226.254:8000/shop";
-// export let authUrlAddr = "https://122.199.226.254:8000/auth";
-// export let mobileUrlAddr = "https://122.199.226.254:8000/mobile";
-// export let adminUrlAddr = "https://122.199.226.254:8000/admin";
-
-//테스트서버용
-// export let shopUrlAddr = "https://was.barodoctor.com/shopTest";
-// export let authUrlAddr = "https://was.barodoctor.com/authTest";
-// export let mobileUrlAddr = "https://was.barodoctor.com/mobileTest";
-// export let adminUrlAddr = "https://was.barodoctor.com/adminTest";
-
-//배포용
-// export let shopUrlAddr = "https://was.barodoctor.com/shop";
-// export let authUrlAddr = "https://was.barodoctor.com/auth";
-// export let mobileUrlAddr = "https://was.barodoctor.com/mobile";
-// export let adminUrlAddr = "https://was.barodoctor.com/admin";
-
 // export let shopUrlAddr = import.meta.env.VITE_SHOP_URL;
 // export let authUrlAddr = import.meta.env.VITE_AUTH_URL;
 // export let mobileUrlAddr = import.meta.env.VITE_MOBILE_URL;
@@ -32,9 +9,8 @@
 // export let mobileUrlAddr = "http://localhost:9000";
 // export let adminUrlAddr = "http://localhost:9000";
 
-export let apiServerAddr = "https://docker.dhdx.kr/senioredudev";
-
 //남원 테스트서버용
+export let apiServerAddr = "https://docker.dhdx.kr/senioredudev";
 export let shopUrlAddr = "https://docker.dhdx.kr/senioredudev";
 export let authUrlAddr = "https://docker.dhdx.kr/senioredudev";
 export let mobileUrlAddr = "https://docker.dhdx.kr/senioredudev";
