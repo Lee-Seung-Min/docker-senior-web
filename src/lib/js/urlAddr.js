c37ed0009513195027be08c2e035eb164ea4191d
j.sohyun <j.sohyun@bit.kr>
2026-01-15 18:57:56 +0900

    26.01.15 서버 URL 정보 업데이트

--- src/lib/js/urlAddr.js
+++ src/lib/js/urlAddr.js
@@ -1,9 +1,5 @@
-// export let shopUrlAddr = import.meta.env.VITE_SHOP_URL;
-// export let authUrlAddr = import.meta.env.VITE_AUTH_URL;
-// export let mobileUrlAddr = import.meta.env.VITE_MOBILE_URL;
-// export let adminUrlAddr = import.meta.env.VITE_ADMIN_URL;
-
 // 남원 로컬개발용
+// export let apiServerAddr = "http://localhost:9000";
 // export let shopUrlAddr = "http://localhost:9000";
 // export let authUrlAddr = "http://localhost:9000";
 // export let mobileUrlAddr = "http://localhost:9000";
@@ -16,18 +12,19 @@
 // export let mobileUrlAddr = "https://docker.dhdx.kr/senior";
 // export let adminUrlAddr = "https://docker.dhdx.kr/senior";
 
-//남원 테스트서버용
-export let apiServerAddr = "https://docker.dhdx.kr/senioredudev";
-export let shopUrlAddr = "https://docker.dhdx.kr/senioredudev";
-export let authUrlAddr = "https://docker.dhdx.kr/senioredudev";
-export let mobileUrlAddr = "https://docker.dhdx.kr/senioredudev";
-export let adminUrlAddr = "https://docker.dhdx.kr/senioredudev";
+// 남원 개발 서버
+// export let apiServerAddr = "https://docker.dhdx.kr/senioredudev";
+// export let shopUrlAddr = "https://docker.dhdx.kr/senioredudev";
+// export let authUrlAddr = "https://docker.dhdx.kr/senioredudev";
+// export let mobileUrlAddr = "https://docker.dhdx.kr/senioredudev";
+// export let adminUrlAddr = "https://docker.dhdx.kr/senioredudev";
 
-// 남원 배포서버용
-// export let shopUrlAddr = "https://docker.dhdx.kr/senioredu";
-// export let authUrlAddr = "https://docker.dhdx.kr/senioredu";
-// export let mobileUrlAddr = "https://docker.dhdx.kr/senioredu";
-// export let adminUrlAddr = "https://docker.dhdx.kr/senioredu";
+// 남원 운영 서버
+export let apiServerAddr = "https://ecare.namwon.go.kr/be";
+export let shopUrlAddr = "https://ecare.namwon.go.kr/be";
+export let authUrlAddr = "https://ecare.namwon.go.kr/be";
+export let mobileUrlAddr = "https://ecare.namwon.go.kr/be";
+export let adminUrlAddr = "https://ecare.namwon.go.kr/be";
 
 export let kakaoRedirectUrlAddr = import.meta.env.VITE_KAKAO_REDIRECT_URL;
 export let googleRedirectUrlAddr = import.meta.env.VITE_GOOGLE_REDIRECT_URL;
