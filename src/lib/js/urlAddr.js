fffe4892437f8cc3537b1e29a12d0c07c6e5a9eb
leesm <seungmin990526@gmail.com>
2026-01-19 17:28:21 +0900

    26.01.19 이승민 URL 오류

--- src/lib/js/urlAddr.js
+++ src/lib/js/urlAddr.js
@@ -1,9 +1,9 @@
 // 남원 로컬개발용
-export let apiServerAddr = "http://localhost:9000";
-export let shopUrlAddr = "http://localhost:9000";
-export let authUrlAddr = "http://localhost:9000";
-export let mobileUrlAddr = "http://localhost:9000";
-export let adminUrlAddr = "http://localhost:9000";
+// export let apiServerAddr = "http://localhost:9000";
+// export let shopUrlAddr = "http://localhost:9000";
+// export let authUrlAddr = "http://localhost:9000";
+// export let mobileUrlAddr = "http://localhost:9000";
+// export let adminUrlAddr = "http://localhost:9000";
 
 
 // 이플마인드 개발 서버
@@ -21,11 +21,11 @@ export let adminUrlAddr = "http://localhost:9000";
 // export let adminUrlAddr = "https://docker.dhdx.kr/senioredudev";
 
 // 남원 운영 서버
-// export let apiServerAddr = "https://ecare.namwon.go.kr/be";
-// export let shopUrlAddr = "https://ecare.namwon.go.kr/be";
-// export let authUrlAddr = "https://ecare.namwon.go.kr/be";
-// export let mobileUrlAddr = "https://ecare.namwon.go.kr/be";
-// export let adminUrlAddr = "https://ecare.namwon.go.kr/be";
+export let apiServerAddr = "https://ecare.namwon.go.kr/be";
+export let shopUrlAddr = "https://ecare.namwon.go.kr/be";
+export let authUrlAddr = "https://ecare.namwon.go.kr/be";
+export let mobileUrlAddr = "https://ecare.namwon.go.kr/be";
+export let adminUrlAddr = "https://ecare.namwon.go.kr/be";
 
 export let kakaoRedirectUrlAddr = import.meta.env.VITE_KAKAO_REDIRECT_URL;
 export let googleRedirectUrlAddr = import.meta.env.VITE_GOOGLE_REDIRECT_URL;
