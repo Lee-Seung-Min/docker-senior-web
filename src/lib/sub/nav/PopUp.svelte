b758c59646dec816dbc7cff2ca172b12c8750fbd
j.sohyun <j.sohyun@bit.kr>
2026-01-21 12:40:52 +0900

    26.01.21 팝업 텍스트 패딩 추가.

--- src/lib/sub/nav/PopUp.svelte
+++ src/lib/sub/nav/PopUp.svelte
@@ -8,7 +8,7 @@
   <section class={sectionClass}>
     <div class="bg" />
     <div class="box_1">
-      <p class="tex"><slot /></p>
+      <p class="tex" style="padding: 10px 0 20px 0"><slot /></p>
       <slot name="btns" />
     </div>
   </section>
